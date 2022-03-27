e1028ed9cf3a3986ab8fab56b48a727ed8060b77
javadbayat <31417320+javadbayat@users.noreply.github.com>
2022-03-27 17:34:20 +0430

    Update thread_host.js
    Added a comment in the code to acknowledge Omegastripes's post in Stack Overflow

--- thread_host.js
+++ thread_host.js
@@ -1,3 +1,13 @@
+// Taking a look at this code still reminds me of Omegastripes's post on Stack Overflow,
+// which illustrated how to use WSH VBS to dynamically create an HTA window, and receive
+// its window object through a Web Browser Control. Here is its link:
+// https://stackoverflow.com/questions/47100085/creating-multi-select-list-box-in-vbscript/47111556#47111556
+// Thanks a lot to his post, I'm using a similar mechanism in this component to
+// transfer thread data (like the thread template element, the extra parameter,
+// the thread flags, .etc) from the mshta.exe process (The HTA process) to the
+// wscript.exe process (the underlying process of the thread) via a dedicated
+// Web Browser Control (WBC). Ha ha!
+
 eval((function() {
     var TF_SUSPENDED        = 1;
     
@@ -5,7 +15,7 @@ eval((function() {
     if (e.atEnd())
         throw new Error("Not enough arguments");
     
-    var appid = "app" + e.item();
+    var signature = "app" + e.item();
     var wins = WScript.CreateObject("Shell.Application").Windows();
     var threadsInfo;
     
@@ -14,7 +24,7 @@ eval((function() {
         var win = wins.Item(i);
         if (win)
         {
-            threadsInfo = win.GetProperty(appid);
+            threadsInfo = win.GetProperty(signature);
             if (threadsInfo)
             {
                 win.RegisterAsBrowser = false;
@@ -24,7 +34,7 @@ eval((function() {
     }
     
     if (!threadsInfo)
-        throw new Error("The property '" + appid + "' does not exist in any windows.");
+        throw new Error("The property '" + signature + "' does not exist in any windows.");
     
     e.moveNext();
     if (e.atEnd())
